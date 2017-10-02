6009a41a3e34000cd6246aaf87478a6193a8a2c9
Stefan Ornik <ornstm13@users.noreply.github.com>
2017-10-02 10:06:31 +0200

    Add files via upload

---
+++ projects/ue02/main.js
@@ -0,0 +1,21 @@
+var x = 2;
+var s;
+var str = "Guten Morgen";
+console.log(x);
+console.log(s);
+s = str;
+var Person = /** @class */ (function () {
+    function Person(nachname, vorname) {
+        this.nachname = nachname;
+        this.vorname = vorname;
+    }
+    Person.prototype.getNachname = function () {
+        return this.nachname;
+    };
+    Person.prototype.getVorname = function () {
+        return this.vorname;
+    };
+    return Person;
+}());
+var p = new Person('Mörth', 'Markus');
+console.log(p.getVorname() + ' ' + p.getNachname());
