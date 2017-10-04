010692bc46beb9f93157eec2dbbfcf4824ff20ea
riedom13 <riedom13@htl-kaindorf.ac.at>
2017-10-04 12:38:37 +0200

    Speckweckerl fortsetzen ue03

--- Projekte/ue03/src/main.ts
+++ Projekte/ue03/src/main.ts
@@ -1,1 +1,32 @@
-console.log('Hello');
+
+class Car {
+    private kennzeichen: string;
+    private _hersteller: string;
+    private modell: string;
+    private baujahr: number;
+    private einsatzbereit: boolean;
+    
+    public constructor (kennzeichen)
+    {
+        this.kennzeichen = kennzeichen;
+    }
+
+public getKennzeichen(): string
+{
+    return this.kennzeichen;
+}
+
+public set hersteller (value: string)
+{
+    this.hersteller = value;
+}
+
+public get hersteller (): string 
+{
+    return this._hersteller;
+}
+}
+const meinAuto = new Car('LB 214 RD');
+meinAuto.hersteller = 'Nissan';
+
+console.log(meinAuto.hersteller);
