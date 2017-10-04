7d6424af539bf38e5d3db9a6d9686665a08bf522
kagfam13 <kagfam13@htl-kaindorf.ac.at>
2017-10-04 12:38:29 +0200

    ue03

--- README.md
+++ README.md
@@ -2,5 +2,6 @@
 
 | Einheit | Datum | Link |
 |---------|-------|------|
-| 1 | 20.09.2017 | [ue01](projects/ue01/main.js)|
-| 2 | 27.09.2017 | [ue02](projects/ue02/main.js) & [ue03](projects/ue03/src/main.ts)|
+| 1 | 20.09.2017 | [ue01](projects/ue01/main.js) |
+| 2 | 27.09.2017 | [ue02](projects/ue02/main.js) & [ue03](projects/ue03/src/main.ts) |
+| 3 | 04.10.2017 | [ue03](projects/ue03/src/main.ts) |0
--- projects/ue03/src/main.ts
+++ projects/ue03/src/main.ts
@@ -1,10 +1,42 @@
 
-import { sprintf } from 'sprintf-js';
+class Car {
+    private kennzeichen: string;
+    private hersteller: string;
+    private modell: string;
+    private baujahr: number;
+    private einsatzbereit: boolean;
 
-class Main {
-    constructor () {
-        console.log('Start');
-    }
+
+	constructor($kennzeichen: string, $hersteller: string, $modell: string, $baujahr: number, $einsatzbereit: boolean) {
+		this.kennzeichen = $kennzeichen;
+		this.hersteller = $hersteller;
+		this.modell = $modell;
+		this.baujahr = $baujahr;
+		this.einsatzbereit = $einsatzbereit;
+	}
+
+	public get $kennzeichen(): string {
+		return this.kennzeichen;
+	}
+    
+
+	public get $hersteller(): string {
+		return this.hersteller;
+	}
+
+	public get $modell(): string {
+		return this.modell;
+	}
+
+	public get $baujahr(): number {
+		return this.baujahr;
+	}
+
+    public get $einsatzbereit(): boolean {
+        return this.einsatzbereit;
+	}
+	
 }
 
-const main = new Main();
+const meinAuto = new Car('GU 310FC', 'Peugeot', '307', 2002, true);
+console.log(meinAuto.$hersteller);
