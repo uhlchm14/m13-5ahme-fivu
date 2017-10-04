fd24f8e9d4a10e8fa3a6b6723a341f29ede37f98
Mario Ritter <ritmam13@htl-kaindorf.ac.at>
2017-10-04 12:38:17 +0200

    update ue03

--- projects/ue03/src/main.ts
+++ projects/ue03/src/main.ts
@@ -1,10 +1,29 @@
+class car {
+    private kennzeichen: string;
+    private hersteller: string;
+    private modell: string;
+    private baujahr: number;
+    private einsatzbereit: boolean;
 
-import { sprintf } from 'sprintf-js';
-
-class Main {
-    constructor () {
-        console.log('Start');
+    public constructor (kennzeichen: string)
+    {
+        this.kennzeichen = kennzeichen; 
     }
+
+    public getKennzeichen (): string{
+        return  this.kennzeichen;
+    }
+
+
+	public set setKennzeichen(value: string) {
+		this.kennzeichen = value;
+    }
+
+	public get getHersteller(): string {
+		return this.hersteller;
+	}
 }
 
-const main = new Main();
+const meinAuto = new car('GU 6666');
+meinAuto.hersteller = 'Seat';
+console.log(meinAuto.getHersteller);
