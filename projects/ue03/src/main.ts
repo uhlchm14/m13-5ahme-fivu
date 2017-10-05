a67c4eba8bdf1aa8487f1a56bfd20a4ed27fdf8e
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-10-05 08:45:14 +0200

    ubuntu is besser

--- projects/ue03/src/main.ts
+++ projects/ue03/src/main.ts
@@ -1,1 +1,48 @@
-console.log("HELLO");
+
+class Car {
+    private _kennzeichen: string;
+    private _hersteller: string;
+    private _modell: string;
+    private _baujahr: number;
+    private _einsatzbereit: boolean;
+
+    public constructor (kennzeichen: string) {
+        this._kennzeichen = kennzeichen;
+    }
+
+    public getKennzeichen (): string {
+        return this._kennzeichen;
+    }
+
+    public set hersteller (value: string) {
+        // if (value === 'Honda') {
+        //     throw new Error('Will ich nicht');
+        // }
+        this._hersteller = value;
+    }
+
+    public get hersteller (): string {
+        return this._hersteller;
+    }
+
+}
+
+const autos: Car [] = []; // new Array
+
+autos.push(new Car('DL 677AW'));
+autos.push(new Car('DL 732BB'));
+autos.push(new Car('DL 5PLB'));
+autos.push(new Car('DL 88HH'));
+
+// alternative ....
+// autos = [ new Car('DL 11111'), new Car('DL 22222')];
+
+for (const a of autos) {
+    console.log(a);
+}
+
+
+
+
+
+console.log(meinAuto.hersteller);
