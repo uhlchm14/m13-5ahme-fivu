e061741a04083ca1b392a7e38cf090bbc4077da1
Mario Ritter <ritmam13@htl-kaindorf.ac.at>
2017-10-05 09:57:00 +0200

    update ue04

--- projects/ue04/src/car.ts
+++ projects/ue04/src/car.ts
@@ -1,10 +1,20 @@
 
-class Car {
+export class Car {
     private _kennzeichen: string;
     private _hersteller: string;
     private _baujahr: number;
 
-    constructor () {
+    constructor (data: ICar) {
+        if (!data.kennzeichen || typeof data.kennzeichen !== 'string') {
+            throw new Error('Kennzeichen fehlt');
+        }
+        this._kennzeichen = data.kennzeichen;
+
+        if (!data.hersteller || typeof data.hersteller !== 'string') {
+            throw new Error('Hersteller fehlt');
+        }
+        this._hersteller = data.hersteller;
+
 
     }
     public get kennzeichen(): string {
@@ -29,5 +39,10 @@ class Car {
     public set baujahr(value: number) {
         this._baujahr = value;
     }
+}
 
+export interface ICar {
+    kennzeichen: string;
+    hersteller: string;
+    baujahr?: number;
 }
--- projects/ue04/src/main.ts
+++ projects/ue04/src/main.ts
@@ -1,2 +1,4 @@
 import * as fs from 'fs';
+import { Car } from './/car';
 
+const car = new Car({kennzeichen: 'GU 12345', hersteller: 'SEAT'});
