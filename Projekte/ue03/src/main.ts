072112d93351da284ebf795eed843bfa92134f59
riedom13 <riedom13@htl-kaindorf.ac.at>
2017-10-05 09:19:05 +0200

    So ein Kack

--- Projekte/ue03/src/main.ts
+++ Projekte/ue03/src/main.ts
@@ -1,3 +1,4 @@
+import * as fs from 'fs';
 
 class Car {
     private kennzeichen: string;
@@ -5,28 +6,51 @@ class Car {
     private modell: string;
     private baujahr: number;
     private einsatzbereit: boolean;
-    
-    public constructor (kennzeichen)
-    {
+
+    public constructor (kennzeichen: string) {
         this.kennzeichen = kennzeichen;
     }
-
-public getKennzeichen(): string
-{
+public getKennzeichen(): string {
     return this.kennzeichen;
 }
 
 public set hersteller (value: string)
 {
+   // if(value === 'Honda')
+    // {
+      //  throw new Error('Will ich nicht');
+    // }
     this.hersteller = value;
 }
 
-public get hersteller (): string 
-{
+public get hersteller (): string {
     return this._hersteller;
 }
 }
-const meinAuto = new Car('LB 214 RD');
-meinAuto.hersteller = 'Nissan';
 
-console.log(meinAuto.hersteller);
+const autos: Car[] = []; // new Array();
+
+autos.push(new Car('LB 214RD'));
+autos.push(new Car('LB 1303R'));
+autos.push(new Car('LB 214DR'));
+autos.push(new Car('LB DRR99'));
+
+// autos = [ new Car('LB DRR99'), new Car('LB 99 DRR') ] // andere Variante
+
+for (const a of autos) {
+    console.log(a);
+}
+
+console.log(autos);
+
+// fs.writeFileSync('C:\\Users\\Dominik\\Documents\\FIV\\m13-5ahme-fivu\\Projekte\\ue03' , JSON.stringify(autos));
+
+try {
+    const buffer = fs.readFileSync('C:\\Users\\Dominik\\Documents\\FIV\\m13-5ahme-fivu\\Projekte\\ue03');
+    const str = buffer.toString();
+    const autos2 = JSON.parse(str);
+    console.log(autos2);
+} catch (err) {
+    console.log('Fehler aufgetreten');
+    console.log(err);
+}
