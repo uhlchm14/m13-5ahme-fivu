93ac166509e9f608c85d11f753d51eccb1652c5f
Mario Ritter <ritmam13@htl-kaindorf.ac.at>
2017-10-05 09:45:11 +0200

    update ue04

---
+++ projects/ue04/src/car.ts
@@ -0,0 +1,33 @@
+
+class Car {
+    private _kennzeichen: string;
+    private _hersteller: string;
+    private _baujahr: number;
+
+    constructor () {
+
+    }
+    public get kennzeichen(): string {
+        return this._kennzeichen;
+    }
+    public get hersteller(): string {
+        return this._hersteller;
+    }
+    public get baujahr(): number {
+        return this._baujahr;
+    }
+
+
+    public set kennzeichen(value: string) {
+        this._kennzeichen = value;
+    }
+
+    public set hersteller(value: string) {
+        this._hersteller = value;
+    }
+
+    public set baujahr(value: number) {
+        this._baujahr = value;
+    }
+
+}
