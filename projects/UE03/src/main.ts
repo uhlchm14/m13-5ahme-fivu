f499fd1a6f9c3b69d3f93dabda2fc669827bf437
woljum13 <woljum13@htl-kaindorf.ac.at>
2017-10-05 08:43:49 +0200

    Wechsel auf Linux

--- projects/UE03/src/main.ts
+++ projects/UE03/src/main.ts
@@ -1,10 +1,41 @@
 
-import { sprintf } from 'sprintf-js';
+class Car {
+    private kennzeichen: string;
+    private _hersteller: string;
+    private modell: string;
+    private baujahr: number;
+    private einsatzbereit: boolean;
 
-class Main {
-    constructor () {
-        console.log('Start');
+    public constructor (kennzeichen: string) {
+        this.kennzeichen = kennzeichen;
+    }
+
+    public getKennzeichen (): string {
+        return this.kennzeichen;
+    }
+
+    public set hersteller (value: string) {
+        this._hersteller = value;
+    }
+
+    public get hersteller (): string {
+        return this._hersteller;
     }
 }
 
-const main = new Main();
+const autos: Car [] = []; // Array ()
+
+autos.push(new Car('DL 11111'));
+autos.push(new Car('DL 22222'));
+autos.push(new Car('DL 33333'));
+autos.push(new Car('DL 44444'));
+
+// autos = [ new Car('DL 11111'), new Car('DL 22222')]; // Alternative Möglichkeit
+
+for (const a of autos) {
+    console.log(a);
+}
+
+// (<any>meinAuto).hersteller = 'Dodge'; // nicht gut :( Type Cast
+// meinAuto.hersteller = 'Honda';
+// console.log(meinAuto.hersteller);
