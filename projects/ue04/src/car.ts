8b7100ff846409a43cb3aa85a10bd165da86008a
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-10-12 09:52:51 +0200

    d

--- projects/ue04/src/car.ts
+++ projects/ue04/src/car.ts
@@ -1,6 +1,7 @@
-export class Car {
+export class Car implements ICar {
   private _kennzeichen: string;
   private _hersteller: string;
+  private _farbe: 'rot' | 'blau' | 'grün';
   private _baujahr: number;
 
   constructor(data: ICar) {
@@ -14,12 +15,19 @@ export class Car {
     }
     this._hersteller = data.hersteller;
 
+    if (data.farbe !== undefined) {
+      if (data.farbe !== 'rot' && data.farbe !== 'grün' && data.farbe !== 'blau') {
+        throw new Error('farbe hat falschen Wert');
+      }
+      this._farbe = data.farbe;
+    }
+
     if (data.baujahr !== undefined) {
       if (typeof data.baujahr !== 'number') {
         throw new Error('Kennzeichen fehlt');
       }
+      this._baujahr = data.baujahr;
     }
-    this._baujahr = data.baujahr;
 
     if (Object.keys(data).length !== Object.keys(this).length) {
       throw new Error('data hat ungültige Attribute');
@@ -36,6 +44,9 @@ export class Car {
   public get baujahr(): number {
     return this._baujahr;
   }
+  public get farbe(): 'rot' | 'blau' | 'grün' {
+    return this._farbe;
+  }
 
   public set kennzeichen(value: string) {
     this._kennzeichen = value;
@@ -46,10 +57,28 @@ export class Car {
   public set baujahr(value: number) {
     this._baujahr = value;
   }
+  public set farbe(f: 'rot' | 'blau' | 'grün') {
+    this._farbe = f;
+  }
+
+  public toObject(): ICar {
+    const rv: ICar = {
+      kennzeichen: this._kennzeichen,
+      hersteller: this._hersteller
+    };
+    if (this._baujahr) {
+      rv.baujahr = this._baujahr;
+    }
+    if (this._farbe) {
+      rv.farbe = this._farbe;
+    }
+    return rv;
+  }
 }
 
 interface ICar {
   kennzeichen: string;
   hersteller: string;
+  farbe?: 'rot' | 'blau' | 'grün';
   baujahr?: number;
 }
--- projects/ue04/src/main.ts
+++ projects/ue04/src/main.ts
@@ -4,5 +4,5 @@ import { Car } from './car';
 
 const car = new Car({ kennzeichen: 'DL 12345', hersteller: 'Honda' });
 
-const carAsJSON = JSON.stringify(car);
+const carAsJSON = JSON.stringify(car.toObject);
 fs.writeFileSync('/home/user/Desktop/car.json', carAsJSON);
