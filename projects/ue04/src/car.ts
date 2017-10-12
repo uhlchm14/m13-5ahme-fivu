b261fe3482f53d75372ed65106dd515d5c6947b2
greflm13 <greflm13@htl-kaindorf.ac.at>
2017-10-12 09:22:33 +0200

    gg

---
+++ projects/ue04/src/car.ts
@@ -0,0 +1,55 @@
+export class Car {
+  private _kennzeichen: string;
+  private _hersteller: string;
+  private _baujahr: number;
+
+  constructor(data: ICar) {
+    if (!data.kennzeichen || typeof data.kennzeichen !== 'string') {
+      throw new Error('Kennzeichen fehlt');
+    }
+    this._kennzeichen = data.kennzeichen;
+
+    if (typeof data.hersteller !== 'string') {
+      throw new Error('Hersteller fehlt');
+    }
+    this._hersteller = data.hersteller;
+
+    if (data.baujahr !== undefined) {
+      if (typeof data.baujahr !== 'number') {
+        throw new Error('Kennzeichen fehlt');
+      }
+    }
+    this._baujahr = data.baujahr;
+
+    if (Object.keys(data).length !== Object.keys(this).length) {
+      throw new Error('data hat ungültige Attribute');
+    }
+    // Object.keys(data).length;
+  }
+
+  public get kennzeichen(): string {
+    return this._kennzeichen;
+  }
+  public get hersteller(): string {
+    return this._hersteller;
+  }
+  public get baujahr(): number {
+    return this._baujahr;
+  }
+
+  public set kennzeichen(value: string) {
+    this._kennzeichen = value;
+  }
+  public set hersteller(value: string) {
+    this._hersteller = value;
+  }
+  public set baujahr(value: number) {
+    this._baujahr = value;
+  }
+}
+
+interface ICar {
+  kennzeichen: string;
+  hersteller: string;
+  baujahr?: number;
+}
--- projects/ue04/src/main.ts
+++ projects/ue04/src/main.ts
@@ -1,1 +1,8 @@
 import * as fs from 'fs';
+
+import { Car } from './car';
+
+const car = new Car({ kennzeichen: 'DL 12345', hersteller: 'Honda' });
+
+const carAsJSON = JSON.stringify(car);
+fs.writeFileSync('/home/user/Desktop/car.json', carAsJSON);
