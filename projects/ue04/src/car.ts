78f5f59be23703397996cb6789e16485983fce41
finmam13 <finmam13@htl-kaindorf.ac.at>
2017-10-18 16:33:35 +0200

    ue04

--- projects/ue04/src/car.ts
+++ projects/ue04/src/car.ts
@@ -1,26 +1,44 @@
-export class Car
+export class Car implements ICar
 {
     private _kennzeichen: string;
     private _hersteller: string;
+    private _farbe: 'rot' | 'blau' | 'grün';
     private _baujahr: number;
 
     constructor(data: ICar)
     {
         if(!data.kennzeichen || typeof data.kennzeichen !== 'string')
         {
-                throw new Error("Kennzeichen fehlt!");
+                throw new Error('Kennzeichen fehlt oder ist leer!');
         }
         this._kennzeichen = data.kennzeichen;
 
-        if(!data.hersteller || typeof data.hersteller !== 'string')
+        if(typeof data.hersteller !== 'string')
         {
-                throw new Error("Hersteller fehlt!");
+                throw new Error('Hersteller fehlt!');
         }
         this._hersteller = data.hersteller;
 
-        if(!data.baujahr || typeof data.baujahr !== 'string')
+        if(data.baujahr !== undefined)
         {
-                throw new Error("Baujahr fehlt!");
+            if(typeof data.baujahr !== 'number')
+            {
+                throw new Error('Baujahr ist keine number!');
+            }
+            this._baujahr = data.baujahr;
+        }
+
+        if(data.farbe !== undefined)
+        {
+            if(data.farbe !== 'rot' && data.farbe !== 'grün' && data.farbe !== 'blau')
+            {
+                throw new Error('Farbe hat falschen Wert');
+            }
+            this._farbe = data.farbe;
+        }
+        if(Object.keys(data).length !== Object.keys(this).length)
+        {
+            throw new Error('data hat ungültige Attribute!');
         }
         // this._baujahr = data.baujahr;
 
@@ -41,6 +59,11 @@ export class Car
         return this._baujahr;
     }
 
+    public get farbe(): 'rot' | 'blau' | 'grün'
+    {
+        return this._farbe;
+    }
+
     public set kennzeichen(value: string)
     {
         this._kennzeichen = value;
@@ -56,11 +79,32 @@ export class Car
         this._baujahr = value;
     } 
 
+    public set farbe(f: 'rot' | 'blau' | 'grün')
+    {
+        this._farbe = f;
+    }
+    public toObject(): ICar
+    {
+        const rv: ICar = {
+            kennzeichen: this._kennzeichen,
+            hersteller: this._hersteller,
+        };
+        if(this._baujahr)
+        {
+            rv.baujahr = this._baujahr;
+        } 
+        if(this._farbe)
+        {
+            rv.farbe = this._farbe;
+        }
+        return  rv;
+    }
 }
 
 export interface ICar // Interface erstellen
 {
     kennzeichen: string;
     hersteller: string;
-    baujahr?: string; // Baujahr muss nicht angegeben werden
+    farbe?: 'rot' | 'blau' | 'grün';
+    baujahr?: number; // Baujahr muss nicht angegeben werden
 }
--- projects/ue04/src/main.ts
+++ projects/ue04/src/main.ts
@@ -1,4 +1,7 @@
 import * as fs from 'fs';
-import {Car} from '.\\car';
+import {Car} from './/car';
 
-const car = new Car ((kennezeichen: 'LB 967EU', hersteller: 'Seat'));
+const car = new Car ({kennzeichen: 'LB 967EU', hersteller: 'Seat'});
+
+const carAsJSON = JSON.stringify(car.toObject());
+fs.writeFileSync('C:/Users/User/Documents/Schule/5AHME/FIVU/ue04/car.json', carAsJSON);
