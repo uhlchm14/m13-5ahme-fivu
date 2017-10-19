9d8164aa34258eb7312fd3312473fe8804757c02
Mario Ritter <ritmam13@htl-kaindorf.ac.at>
2017-10-19 09:18:57 +0200

    update ue05 and docs

--- projects/ue05/src/main.ts
+++ projects/ue05/src/main.ts
@@ -1,4 +1,6 @@
+
 import {sprintf} from 'sprintf-js';
+import * as express from 'express'; // express importieren
 
 class Main {
     constructor () {
@@ -7,3 +9,13 @@ class Main {
 }
 
 const main = new Main();
+
+// Express Web Server erstellen
+const server = express();
+
+// Verzeichnis public für statische HTML Seiten erstellen
+server.use(express.static('public'));
+
+server.listen(4711); // Port des Servers = 4711
+
+
