11de07b4c681271add9b1209ee923d0af3954a5d
Mario Ritter <ritmam13@htl-kaindorf.ac.at>
2017-11-16 09:49:20 +0100

    update ue06

--- projects/ue06/src/main.ts
+++ projects/ue06/src/main.ts
@@ -13,38 +13,16 @@ const main = new Main();
 // Express Web Server erstellen
 const server = express();
 // Rendering engine pug in Express einbinden
-server.set('view engine', 'pug');
-
-let counter = 1;
-// Express arbeitet in Schichten nach der Reihenfolge der Definition im Quelltext
-// Erste Schicht
-server.get('/test', (req, res, next) => { // Ressource: /test
-    // console.log(req);
-    let html = '';
-    html += '<!DOCTYPE html>';
-    html += '<html>';
-    html += '<head>';
-    html += '  <title>Meine Hompage</title>';
-    html += '  <link rel="stylesheet" href="myStyle.css" />';
-    html += '</head>';
-    html += '<body>';
-    html += '  <h1> Meine Hompage</h1>';
-    html += '  <h2 id=absatz2test>' + counter++ + '.Aufruf</h2>';
-    html += '  <p>Das ist eine Testseite</p>';
-    html += '  <p id="absatz2"> Das ist der 2te Absatz</p>';
-    html += '  <p class="gelbeAbsaetze"> Das ist der 2te Absatz</p>';
-    html += '</body>';
-    html += 'html>';
-    res.send(html);
-});
-
-server.get('/pug', (req, res, next) => {
-    res.render('test.pug', {counterValue: counter++});
+const pugRenderingEngine = server.set('view engine', 'pug');
+pugRenderingEngine.locals.pretty = true;
+server.get('/', (req, res, next) => {
+    // res.render('index.pug');
+    next();
 });
 // 2 Schicht
 // Verzeichnis public für statische HTML Seiten erstellen
 server.use(express.static('public'));
 
 server.listen(4711); // Port des Servers = 4711
-console.log('Server auf Port 4711 gestartet');
+console.log('Server auf Port 4711 gestartet (http://localhost:4711)');
 
