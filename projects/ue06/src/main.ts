738572e92e21ec7cb4a7137713f7a8a4f95c6353
Lukas <frelum13@htl-kaindorf.ac.at>
2017-11-23 09:52:10 +0100

    Einheit 10

--- projects/ue06/src/main.ts
+++ projects/ue06/src/main.ts
@@ -1,7 +1,10 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as cookie from 'cookie';
 
 class Main {
-    constructor (){
+    constructor ()
+    {
         console.log('Start');
     }
 
@@ -16,8 +19,32 @@ PugRenderingEngine.locals.pretty = true;
 
 // Express arbeitet in Schichten nach der Reihenfolge der Definition im Quelltext
 
+server.use(bodyParser.urlencoded());
+
+// 1 Schicht
 server.get('/', (req, res, next) => {
     // res.render('index.pug');
+    next();
+});
+
+// 2 Schicht
+server.post('/saveuser', (req, res, next) => {
+    // res.render('index.pug');
+    if (req.body) {
+        console.log(req.body);
+    }
+
+    if (req.body.name === 'maxi' && req.body.password === 'geheim') {
+        res.setHeader(
+            'Set-Cookie', 
+            cookie.serialize('name', req.body.name, {
+            httpOnly: true,
+            maxAge: 60 * 60 * 24 * 7 // 1 week
+          }));
+        res.send('OK');
+    } else {
+        res.status(401).send('Error');
+    }
     next();
 });
 
