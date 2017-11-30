48cca52a58b357cc96938580d23dce400655725b
Mario Ritter <ritmam13@htl-kaindorf.ac.at>
2017-11-30 10:03:21 +0100

    update project ue07

--- projects/ue07/src/main.ts
+++ projects/ue07/src/main.ts
@@ -1,9 +1,8 @@
 
-import {sprintf} from 'sprintf-js';
-import * as express from 'express'; // express importieren
+import { sprintf } from 'sprintf-js';
+import * as express from 'express';
 import * as bodyParser from 'body-parser';
-// import * as cookie from 'cookie';
-// import * as cookieParser from 'cookie-parser';
+
 class Main {
     constructor () {
         console.log('Start');
@@ -14,43 +13,42 @@ const main = new Main();
 
 // Express Web Server erstellen
 const server = express();
+
 // Rendering engine pug in Express einbinden
 const pugRenderingEngine = server.set('view engine', 'pug');
 pugRenderingEngine.locals.pretty = true;
+
+// Express arbeitet in Schichten nach der Reihefolge der
+// Definition im Quelltext
+
+
 server.use(bodyParser.urlencoded());
-server.use(cookieParser());
-// 1.Schicht
+
+// 1. Schicht
 server.get('/', (req, res, next) => {
     // res.render('index.pug');
-    console.log(req.cookies);
-    if (req.cookies && req.cookies.name === 'max') {
-        console.log('Anfrage von Maxi');
-    }
     next();
 });
-// 2.Schicht
-server.post('/saveuser', (req, res, next) => {
+
+// 2. Schicht
+server.post('/login', (req, res, next) => {
     // res.render('index.pug');
     if (req.body) {
         console.log(req.body);
     }
-    if (req.body.name === 'max' && req.body.password === 'geheim') {
-        // res.setHeader(
-        //     'Set-Cookie',
-        //     cookie.serialize('name', req.body.name, {
-        //         httpOnly: true,
-        //         maxAge: 60 * 60 * 24 * 7 // 1 week
-        //     })
-        // );
-        res.send('Supa (' + req.body.email + ')');
+    if (req.body.name === 'maxi' && req.body.password === 'geheim') {
+        res.send('OK (' + req.body.name + ')');
     } else {
-        res.status(401).send('Error');
+        res.status(401).send('ERROR');
     }
-    next();
+    // next();
 });
+
 // 3. Schicht
-// Verzeichnis public für statische HTML Seiten erstellen
+// Verzeichnis public für statische HTML Seiten definieren
 server.use(express.static('public'));
 
-server.listen(4711); // Port des Servers = 4711
-console.log('Server auf Port 4711 gestartet (http://localhost:4711)');
+
+server.listen(4711);
+console.log('Server gestartet (http://localhost:4711)')
+
