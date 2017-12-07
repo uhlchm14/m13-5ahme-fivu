f8fdc028594cf98decae6db395f57739d6394cf3
Mario Ritter <ritmam13@htl-kaindorf.ac.at>
2017-12-07 09:56:04 +0100

    update ue07

--- projects/ue07/src/main.ts
+++ projects/ue07/src/main.ts
@@ -5,9 +5,11 @@ import * as bodyParser from 'body-parser';
 import * as jwt from 'jsonwebtoken';
 import * as fs from 'fs';
 import * as path from 'path';
+import { decode } from 'jsonwebtoken';
 
 class Main {
 
+    private _server: express.Express;
     private _privateKey: Buffer;
     private _publicKey: Buffer;
     constructor () {
@@ -19,49 +21,62 @@ class Main {
         this._privateKey = fs.readFileSync(privateFileName);
         console.log('reading private Key from : ' + privateFileName);
         console.log('Key-Pair read from files');
+
+        // Express Web Server erstellen
+        this._server = express();
+
+        this._server.use(bodyParser.urlencoded({extended: true}));
+        
+        // 1. Schicht
+        this._server.get('/', (req, res, next) => {
+            next();
+        });
+        
+        // 2. Schicht
+        this._server.post('/login', (req, res, next) => this.getLogin(req,res,next));
+        // 3. Schicht
+        this._server.get('/data', (req, res, next) => this.getData(req,res,next))
+        // 4. Schicht
+        this._server.use(express.static('public'));
+        
+    }
+
+    public startServer(port: number) {
+        this._server.listen(port);
+        console.log('Server gestartet (http://localhost:4711)')
+    }
+    private getLogin ( req: express.Request, res: express.Response, next: express.NextFunction) {
+        if (req.body) {
+            console.log(req.body);
+        }
+        if (req.body.name === 'maxi' && req.body.password === 'geheim') {
+            const token = jwt.sign({
+                                        name: 'maxi'},
+                                        this._privateKey, 
+                                        { expiresIn: '10min', algorithm: 'RS256'}
+                                    );
+            console.log(token);
+            res.json({ token: token});
+            // res.send('OK (' + req.body.name + ')');
+        } else {
+            res.status(401).send('ERROR');
+        }
+        // next();
+    }
+
+    private getData ( req: express.Request, res: express.Response, next: express.NextFunction) {
+        const value = <string>req.headers.authorization; 
+        if(value.startsWith('Bearer: ')){
+            const token = value.substr(8);
+            console.log(token);
+            jwt.verify(token,this._publicKey,(err, decoded) => {
+                console.log(decoded);
+            });
+        }
+        res.status(500).send('Not implemented');
     }
 }
 
 const main = new Main();
-
-// Express Web Server erstellen
-const server = express();
-
-// Rendering engine pug in Express einbinden
-const pugRenderingEngine = server.set('view engine', 'pug');
-pugRenderingEngine.locals.pretty = true;
-
-// Express arbeitet in Schichten nach der Reihefolge der
-// Definition im Quelltext
-
-
-server.use(bodyParser.urlencoded());
-
-// 1. Schicht
-server.get('/', (req, res, next) => {
-    // res.render('index.pug');
-    next();
-});
-
-// 2. Schicht
-server.post('/login', (req, res, next) => {
-    // res.render('index.pug');
-    if (req.body) {
-        console.log(req.body);
-    }
-    if (req.body.name === 'maxi' && req.body.password === 'geheim') {
-        res.send('OK (' + req.body.name + ')');
-    } else {
-        res.status(401).send('ERROR');
-    }
-    // next();
-});
-
-// 3. Schicht
-// Verzeichnis public für statische HTML Seiten definieren
-server.use(express.static('public'));
-
-
-server.listen(4711);
-console.log('Server gestartet (http://localhost:4711)')
+main.startServer(4711);
 
