94415ef53967f14c765f8e8aab397e45e9d82c3f
greflm13 <greflm13@htl-kaindorf.ac.at>
2018-01-11 09:52:23 +0100

    11.1.2018

--- projects/ue08-FirstAngular/src/app/app.component.ts
+++ projects/ue08-FirstAngular/src/app/app.component.ts
@@ -1,22 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   public title = 'your anus';
-  private width = 1;
+  public width = 1;
+  public show = false;
+  public time: string;
+  private interval: number;
 
-  constructor() {}
+  constructor() { }
 
   public ngOnInit(): void {
     setTimeout(() => {
       this.title = 'uranus';
     }, 500);
-    setInterval(() => {
+    const interval = setInterval(() => {
       this.width++;
-    }, 100);
+      if (this.width > 500) {
+        clearInterval(interval);
+        this.show = true;
+      };
+    }, 10);
+    this.updateTime();
+    this.interval = window.setInterval(() => {
+      this.updateTime();
+    }, 100)
+  }
+
+  public ngOnDestroy () {
+    if (this.interval !== undefined) {
+      clearInterval(this.interval);
+      this.interval = undefined;
+    }
+  }
+
+  public reload(): void {
+    location.reload();
+  }
+
+  private updateTime() {
+    this.time = new Date().toLocaleTimeString();
   }
 }
