1ac8ecbd482f42f6616bd263dbf9f20afecac35a
Mario Ritter <ritmam13@htl-kaindorf.ac.at>
2018-01-11 09:50:38 +0100

    update ue08

--- projects/ue08/src/app/app.component.ts
+++ projects/ue08/src/app/app.component.ts
@@ -1,20 +1,32 @@
 import { Component, OnInit} from '@angular/core';
-import { setTimeout } from 'timers';
+import { OnDestroy } from '@angular/core/src/metadata/lifecycle_hooks';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
   public title = 'ue08';
+  public time: string;
+  private _timer: number;
 
   constructor () {
   }
   public ngOnInit () {
     console.log('ngOnInit');
-    setTimeout( () => {
-      this.title = 'Hallo';
-    }, 5000);
+    this.updateTime();
+    this._timer = window.setInterval( () => this.updateTime(), 100);
+  }
+  public ngOnDestroy () {
+    if (this._timer !== undefined) {
+      window.clearInterval(this._timer);
+      this._timer = undefined;
+    }
+  }
+  private updateTime() {
+    const now = new Date();
+    this.time = now.toLocaleTimeString();
   }
 }
