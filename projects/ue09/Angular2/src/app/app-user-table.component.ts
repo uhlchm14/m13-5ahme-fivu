bf94c59434d7f77147d20d957cf7d32a9494265e
Kager <kagfam13@htl-kaindorf.ac.at>
2018-01-26 10:11:49 +0100

    2501

--- projects/ue09/Angular2/src/app/app-user-table.component.ts
+++ projects/ue09/Angular2/src/app/app-user-table.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {IUser} from './models/user';
 import {UserService} from './user.service';
 
@@ -7,11 +7,26 @@ import {UserService} from './user.service';
   templateUrl: './app-user-table.component.html',
 })
 
-export class AppUserTableComponent {
-  public users: IUser[] = [];
+export class AppUserTableComponent implements OnInit {
+  public users: IUser[];
+  public userErr: Error;
 
   constructor (private userService: UserService) {
-      this.users = this.userService.getUsers();
+    this.userService = userService;
+    // this.userService.getUsers().then (users => {
+    // this.users = users;
+    // }).catch (err => {
+    //  console.log(err);
+    // });
+  }
+
+  public async ngOnInit () {
+    try {
+      this.users = await this.userService.getUsers();
+    } catch (err) {
+      this.userErr = err;
+      console.log(err);
+    }
   }
 
   public onClickUser(u: IUser) {
--- projects/ue09/Angular2/src/app/user.service.ts
+++ projects/ue09/Angular2/src/app/user.service.ts
@@ -9,9 +9,18 @@ export class UserService {
     this.users.push({surname: 'Freyler', firstname: 'Lukas', classname: '5AHME'});
     this.users.push({surname: 'Kager', firstname: 'Fabian', classname: '5AHME'});
     this.users.push({surname: 'Ritter', firstname: 'Mario', classname: '5AHME'});
+    this.users.push({surname: 'Fink', firstname: 'Matthias', classname: '5AHME'});
+    this.users.push({surname: 'Ornik', firstname: 'Stefan', classname: '5AHME'});
   }
 
-  public getUsers (): IUser [] {
-    return [];
+  public async getUsers (): Promise<IUser []> {
+    return new Promise<IUser []>( (resolve, reject) => {
+      setTimeout( () => {
+        resolve(this.users);
+        // reject(new Error('Users not available'));
+      }, 2000);
+    });
+    // return this.users;
+    // throw new Error('No Connection');
   }
 }
