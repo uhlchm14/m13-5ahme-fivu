9beb355169d131426b8a0ee45c963d293670bcec
Mario Ritter <ritmam13@htl-kaindorf.ac.at>
2018-02-01 09:39:38 +0100

    update ue10

--- projects/ue10/src/main.ts
+++ projects/ue10/src/main.ts
@@ -5,15 +5,18 @@ import * as https from 'https';
 
 
 
-let privateKey  = fs.readFileSync('certificates/ca.crt', 'utf8');
-let certificate = fs.readFileSync('certificates/ca.pem', 'utf8');
+let privateKey  = fs.readFileSync('certificates/ca.pem', 'utf8');
+let certificate = fs.readFileSync('certificates/ca.crt', 'utf8');
 let credentials = {key: privateKey, cert: certificate};
-let app = express();
+let server = express();
 
+server.get('/', (req, res, next) => {
+    res.send('OK');
+});
 // your express configuration here
 
-let httpServer = http.createServer(app);
-let httpsServer = https.createServer(credentials, app);
+let httpServer = http.createServer(server);
+let httpsServer = https.createServer(credentials, server);
 
 httpServer.listen(8080);
 httpsServer.listen(8443);
