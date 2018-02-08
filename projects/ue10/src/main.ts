9aaae3cefb4dc5da0bf04d2a7925c200ee8eef39
Kager <kagfam13@htl-kaindorf.ac.at>
2018-02-08 09:50:27 +0100

    Penis

--- projects/ue10/src/main.ts
+++ projects/ue10/src/main.ts
@@ -1,61 +1,34 @@
 import * as fs from 'fs';
+import * as express from 'express';
+import * as https from 'https';
+import * as http from 'http';
 
-class Car {
-    private kennzeichen: string;
-    private hersteller: string;
-    private modell: string;
-    private baujahr: number;
-    private einsatzbereit: boolean;
+class Main {
+    private _server: express.Express;
+    private _zertifikat: Buffer;
+    private _privatekey: Buffer;
 
+    constructor () {
+        this._zertifikat = fs.readFileSync('../x509/server-kager.crt');
+        this._privatekey = fs.readFileSync('../x509/server-kager.pem');
 
-    constructor($kennzeichen: string, $hersteller: string, $modell: string, $baujahr: number, $einsatzbereit: boolean) {
-        this.kennzeichen = $kennzeichen;
-        this.hersteller = $hersteller;
-        this.modell = $modell;
-        this.baujahr = $baujahr;
-        this.einsatzbereit = $einsatzbereit;
+        this._server = express();
+
+        // const credentials = {key: this._privatekey, cert: this._zertifikat};
     }
-
-    public get $kennzeichen(): string {
-        return this.kennzeichen;
+    public startHTTPServer(port: number) {
+        const httpServer = http.createServer(this._server);
+        httpServer.listen(port);
+        console.log('Server gestartet (http://localhost:8080)');
     }
-
-    public get $hersteller(): string {
-    return this.hersteller;
-    }
-
-    public get $modell(): string {
-        return this.modell;
-    }
-
-    public get $baujahr(): number {
-        return this.baujahr;
-    }
-
-    public get $einsatzbereit(): boolean {
-        return this.einsatzbereit;
+    public startHTTPSServer(port: number) {
+        const credentials = {key: this._privatekey, cert: this._zertifikat};
+        const httpsServer = https.createServer(credentials, this._server);
+        httpsServer.listen(port);
+        console.log('Server gestartet (http://localhost:8443)');
     }
 }
 
-const autos: Car [] = [];
-
-autos.push(new Car('GU 310FC', 'Peugeot', '307', 2002, true));
-autos.push(new Car('GU 715KN', 'Seat', 'Altea', 2006, true));
-autos.push(new Car('LB 310EU', 'Seat', 'Ibiza', 2008, true));
-autos.push(new Car('LAU CH01', 'Tesla', '3S', 2017, true));
-
-// fs.writeFileSync('C:\\Schule\\5AHME\\FIVU\\projects\\m13-5ahme-fivu\\projects\\ue03\\autos.json', JSON.stringify(autos));
-
-try {
-    const buffer = fs.readFileSync('C:\\Schule\\5AHME\\FIVU\\projects\\m13-5ahme-fivu\\projects\\ue03\\autos.json');
-    const str = buffer.toString();
-    const autos2 = JSON.parse(str);
-    console.log(autos2);
-} catch (err) {
-    console.log('Fehler');
-    console.log(err);
-
-}
-
-
-console.log(autos);
+const main = new Main();
+main.startHTTPServer(8080);
+main.startHTTPSServer(8443);
