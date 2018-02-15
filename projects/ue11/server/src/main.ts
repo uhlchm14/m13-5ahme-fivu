c0d08604680a6a2a8fd52367f6a98025ef1567ae
frelum13 <frelum13@htl-kaindorf.ac.at>
2018-02-15 09:50:40 +0100

    15.2.18

--- projects/ue11/server/src/main.ts
+++ projects/ue11/server/src/main.ts
@@ -1,52 +1,73 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+import * as nconf from 'nconf';
+import * as path from 'path'; // Node.js Modul
+import * as http from 'http';
+
 
 class Main {
-    constructor (){
-        console.log('Start');
+
+    private _express: express.Express;
+    private _config: {port: number};
+    private _server: http.Server;
+
+    constructor () {
+        const configFile = path.join(__dirname, '..', 'config.json');
+        console.log(configFile);
+        nconf.file(configFile);
+        this._config = nconf.get('server');
+        if (!this._config || isNaN(this._config.port)) {
+            throw new Error('invalid configuration');
+        }
     }
 
+    public async start (): Promise<void> {
+        this._express = express();
+        this._express.use(bodyParser.urlencoded());
+        // const pugRenderingEngine = this._server.set('view engine', 'pug');
+        // pugRenderingEngine.locals.pretty = true;
+
+        this._express.get(['/', '/index.html', '/index.htm'], (req, res, next) => this.handleGetAll(req, res, next));
+        this._express.get('/error', (req, res, next) => this.handleGetError(req, res, next));
+        this._express.use(
+            (err: any,
+             req: express.Request,
+             res: express.Response,
+             next: express.NextFunction) => this.handleError(err, req, res, next)
+        );
+
+        this._server = http.createServer(this._express);
+        this._server.listen(this._config.port);
+        console.log('Server now listening (http://localhost:' + this._config.port + ')');
+    }
+
+    private handleGetAll (req: express.Request, res: express.Response, next: express.NextFunction) {
+        try {
+            console.log('get request from client');
+            res.send('starting angular');
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    private handleGetError (req: express.Request, res: express.Response, next: express.NextFunction) {
+        try {
+            throw new Error('Test');
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    private handleError(err: any, req: express.Request, res: express.Response, next: express.NextFunction) {
+        res.status(500).send('internal Error');
+    }
 }
 
-const main = new Main();
-// Express Web Server erstellen
-const server = express();
-// Rendering engine pug in Express einbinden
-server.set('view engine', 'pug');
-// Express arbeitet in Schichten nach der Reihenfolge der Definition im Quelltext
+async function startup() {
+    const main = new Main();
+    await main.start();
+}
 
-let counter = 1;
-
-// 1.Schicht
-// => error operator
-server.get('/test', (req, res, next) => {
-    // console.log(req);
-    // next(); wird nicht aufgerufen weil wir behandeln die abfrage
-    let html = '';
-
-    html += '<!DOCTYPE html>';
-    html += '<html>';
-    html += '<head>';
-    html += '<title>Meine Testseite</title>';
-    html += '<link rel= "stylesheet" href = "mystyle.css" />';
-    html += '</head>';
-    html += '<body>';
-    html += '<h1>Test</h1>';
-    html += '<h2>' + counter++ + '. Aufruf</h2>';
-    html += '<p id = "absatz2">Hallo mein Name ist</p>';
-    html += '<p>Hallo mein Name ist</p>';
-    html += '<p class = "gelbeAbsaetzte">Hallo mein Name ist</p>';
-    html += '</body>';
-    html += '</html>';
-
-    res.send(html);
+startup().catch( (err) => {
+    console.log(err);
 });
-
-server.get('/pug', (req, res, next) => {
-    res.render('test.pug', {counterValue: counter++});
-});
-
-// 2.Schicht
-server.use(express.static('public'));
-
-
-server.listen(4711);
