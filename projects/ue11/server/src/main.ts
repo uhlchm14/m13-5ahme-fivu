1c9606686cc4580b257a181d435091f61596a0c5
Kager <kagfam13@htl-kaindorf.ac.at>
2018-02-15 08:53:42 +0100

    run

--- projects/ue11/server/src/main.ts
+++ projects/ue11/server/src/main.ts
@@ -1,9 +1,19 @@
+import * as express from 'express';
+import * as bodyParser from 'body-parser'
 
 import { sprintf } from 'sprintf-js';
 
 class Main {
+
+    private _server: express.Express;
+
     constructor () {
         console.log('Start');
+    }
+
+    public async start(): Promise<void>{
+        this._server = express();
+        this._server.use(bodyParser.urlencoded());
     }
 }
 
