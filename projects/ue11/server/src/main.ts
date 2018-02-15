82dcd8037d56b3b7098d019da8039f004de176b1
Kager <kagfam13@htl-kaindorf.ac.at>
2018-02-15 09:51:54 +0100

    done

--- projects/ue11/server/src/main.ts
+++ projects/ue11/server/src/main.ts
@@ -1,20 +1,69 @@
 import * as express from 'express';
-import * as bodyParser from 'body-parser'
-
+import * as bodyParser from 'body-parser';
+import * as nconf from 'nconf';
+import * as path from 'path';
+import * as http from 'http';
 import { sprintf } from 'sprintf-js';
 
 class Main {
 
-    private _server: express.Express;
+    private _express: express.Express;
+    private _config: {port: number};
+    private _server: http.Server;
 
     constructor () {
-        console.log('Start');
+        const configFile = path.join(__dirname, '..', 'config.json');
+        console.log(configFile);
+        nconf.file(configFile);
+        this._config = nconf.get('server');
+        if (!this._config || isNaN(this._config.port)) {
+            throw new Error('invalid configuration');
+        }
     }
 
     public async start(): Promise<void>{
-        this._server = express();
-        this._server.use(bodyParser.urlencoded());
+        this._express = express();
+        this._express.use(bodyParser.urlencoded());
+
+        this._express.get(['/', '/index.html', 'index.htm'], (req, res, next) => this.handleGetAll(req, res, next));
+        this._express.get(['/error'], (req, res, next) => this.handleGetError(req, res, next));
+
+        this._express.use((err: any, req: express.Request, res: express.Response, 
+            next: express.NextFunction) => this.handleError(err, req, res, next));
+        
+        this._server = http.createServer(this._express);
+        this._server.listen(this._config.port);
+        console.log('server now listening to ( http://localhost:' + this._config.port + ' )');
+    }
+
+    private handleGetAll(req: express.Request, res: express.Response, 
+        next: express.NextFunction) {
+        try {
+            console.log('get request from client');
+            res.send('Not implemented yet');
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    private handleGetError(req: express.Request, res: express.Response, 
+        next: express.NextFunction) {
+        try {
+            throw new Error('Testing error');
+        } catch (err) {
+            next(err);
+        }
+    }
+
+    private handleError(err: any, req: express.Request, res: express.Response, 
+        next: express.NextFunction) {
+            res.status(500).send('internal Error: \n' + err);
     }
 }
-
-const main = new Main();
+async function startup(): Promise<void> {
+    const main = new Main();
+    await main.start();
+}
+startup().catch((err)=> {
+    console.log(err);
+});
